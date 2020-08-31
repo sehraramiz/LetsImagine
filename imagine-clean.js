dada6fc9b56944965c4fe436a0f169e40493ff4a
Mohsen Barzegar <mohsennbarzegar@gmail.com>
2020-08-31 14:41:14 +0430

    a little change in style cleaned the message to be more family friendly

--- imagine-clean.js
+++ imagine-clean.js
@@ -8,33 +8,33 @@ var css = `
     #box {
       background-color: #fff;
       font-family: arial, "Times New Roman";
-      font-size: 20px;
+      font-size: x-large;
       color: #555555;
       border: 5px solid pink;
       padding: 40px;
       margin: 30px;
       text-align: center;
-      line-height: 1.8;
+      line-height: 2;
       letter-spacing: 1px;
       word-spacing: 0.5px;
  	}
-    
+
     a {
       color: #FA8072;
       text-decoration: none;'
       animation: heartbeat 5s infinite;
       font-weight: bold;
     }
-    
+
     a : visited {
       color: #FA8072;
   	}
-    
+
     span {
       font-weight: bold;
       font-size: 22px;
     }
-    
+
     h1 {
     	animation: heartbeat 4s infinite;
         color: #000;
@@ -83,10 +83,9 @@ var bodyContent = `
        <div>You think you are fine with your internet browser's private tab and some vpn?</div>
        <h3 class="title">We offer a better solution!</h3>
        <div><span>99%</span> privacy guaranteed !</div>
-       <div>Why jerk off to some cheap porn star,<br>when you can have a good time with your favorite movie star.</div>
-       <h3 class="title">Just Use Your <a href="https://en.wikipedia.org/wiki/Imagination">Imagination</a><br><span>instead of porn</span></h3>
+       <h3 class="title">Just Use Your <a href="https://en.wikipedia.org/wiki/Imagination">Imagination</a></h3>
        <div>Join the <span>Imaginative</span> side</div>
-       <div>a place where only <span>god</span>, <span>you</span>, and a <span>mind reader</span><br>can know about your ugly, fucked up desires.</div>
+       <div>a place where only <span>god</span>, <span>you</span>, and a <span>mind reader</span><br>can know about your ugly, dirty desires.</div>
     </div>
 
 `;
